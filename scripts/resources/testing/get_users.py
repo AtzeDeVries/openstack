115b540a7def381370ca6829a651b4c018034c08
Atze de Vries <atze.devries@naturalis.nl>
2015-10-23 10:58:32 +0200

    fix of '

--- scripts/resources/testing/get_users.py
+++ scripts/resources/testing/get_users.py
@@ -9,7 +9,7 @@ from os import environ
 auth = v3.Password(auth_url=environ['OS_AUTH_URL'],
                    username=environ['OS_USERNAME'],
                    password=environ['OS_PASSWORD'],
-                   project_name=environ['OS_PROJECT_NAME']
+                   project_name=environ['OS_PROJECT_NAME'],
                    user_domain_name='Default')
 
 sess = session.Session(auth=auth)
