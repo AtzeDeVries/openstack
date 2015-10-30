fcf36f815fb364414a4cc9680b5e6974628585dc
Atze de Vries <atze.devries@naturalis.nl>
2015-10-30 16:30:31 +0100

    added ad sync framework

--- scripts/resources/testing/get_users.py
+++ scripts/resources/testing/get_users.py
@@ -1,26 +1,21 @@
 #!/usr/bin/env python2.7
-
 from keystoneclient.auth.identity import v3
 from keystoneclient import session
 from keystoneclient.v3 import client
 from os import environ
 
-
-auth = v3.Password(auth_url=environ['OS_AUTH_URL'],
+auth = v3.Password(auth_url="https://stack.naturalis.nl:5000/v3",
                    username=environ['OS_USERNAME'],
                    password=environ['OS_PASSWORD'],
                    project_name=environ['OS_PROJECT_NAME'],
-                   user_domain_name='Default')
+                   user_domain_name='Default',
+                   project_domain_id='Default')
 
-sess = session.Session(auth=auth)
-#
-#
-# ks = client.Client(session=sess)
-# users = ks.users.list()
-# print users
+sess = session.Session(auth=auth,verify='~/projects/stack.naturalis.nl/cert/stack_naturalis_nl.ca-bundle')
 
-a = sess.get('/v3/users',
-             endpoint_filter={'service_type': 'identity',
-                              'interface': 'internalURL',
-                              'region_name': 'RegionOne'})
-print a
+ks = client.Client(session=sess)
+users = ks.users.list()
+for u in users:
+        print u.name
+groups = ks.groups.list()
+print groups
