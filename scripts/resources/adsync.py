d2d234323efedb1bac202d4e112099799f4b27d5
Atze de Vries <atze.devries@naturalis.nl>
2015-11-03 12:55:36 +0100

    cleanup again()

--- scripts/resources/adsync.py
+++ scripts/resources/adsync.py
@@ -37,41 +37,13 @@ c = ad.connect(host,domain+user,password)
 ksclient = ks.connect(auth_url,ks_username,ks_password,project_name)
 
 
-# def add_users_objects(ad_user_object,keystone):
-#     return [x for x in ad_user_object if x['username'] not in keystone]
-#
-# def disable_users(ad_user_object,keystone):
-#     #adusers = [u['username'] for u in ad_user_object ]
-#     return [x for x in keystone if x not in [u['username'] for u in ad_user_object]]
-
-# def added_groups(ad_flat_groups,ks_groups):
-#     return [x for x in ad_flat_groups if x not in ks_groups]
-#
-# def removed_groups(ad_flat_groups,ks_groups):
-#     return [x for x in ks_groups if x not in ad_flat_groups]
-
 def sync_users():
-    pass
 
-def sync_groups():
-    pass
-
-def sync_membership():
-    pass
-
-
-if c.bind():
-    log.logger.info('Starting user sync')
     all_users = ad.users_in_group(c,'Openstack - All Users')
     ks_users = [u.name for u in ksclient.users.list(group=ks_ad_group_sync_id,enabled=True)]
     ks_users_disabled = [u.name for u in ksclient.users.list(group=ks_ad_group_sync_id,enabled=False)]
     added_users = [x for x in all_users if x['username'] not in ks_users]
     disabled_users = [x for x in ks_users if x not in [q['username'] for q in all_users]]
-    ks_group_list = [g.name for g in ksclient.groups.list() if g.name[:17] == 'Research Group - ']
-    #ad_added_groups = added_groups(ad.gather_ad_groups(c),ks_group_list)
-    ad_added_groups = [x for x in ad.gather_ad_groups(c) if x not in ks_group_list]
-    #ad_removed_groups = removed_groups(ad.gather_ad_groups(c),ks_group_list)
-    ad_removed_groups = [x for x in ks_group_list if x not in ad.gather_ad_groups(c)]
 
     for u in added_users:
         log.logger.debug("Checking if %s already exists : %s" % ( u['username'], str(ks.user_exists(ksclient,u['username']))))
@@ -86,14 +58,21 @@ if c.bind():
     for u in disabled_users:
         log.logger.info("disable user: %s" % u)
 
-    log.logger.info('Starting group sync')
+
+def sync_groups():
+
+    ks_group_list = [g.name for g in ksclient.groups.list() if g.name[:17] == 'Research Group - ']
+    ad_added_groups = [x for x in ad.gather_ad_groups(c) if x not in ks_group_list]
+    ad_removed_groups = [x for x in ks_group_list if x not in ad.gather_ad_groups(c)]
+
     for g in ad_added_groups:
         log.logger.info("add group %s" % g)
 
     for g in ad_removed_groups:
         log.logger.info("remove group %s" % g)
 
-    log.logger.info('Starting sync users in groups')
+def sync_membership():
+
     for i in ad.groups_in_group(c,'Openstack - All Users'):
         log.logger.debug("Group: Research Group - %s" % i[12:])
         users_ad = [ u['username'] for u in ad.users_in_group(c,i) ]
@@ -107,4 +86,11 @@ if c.bind():
             log.logger.warning("Group %s does not available!" % i)
 
 
+if c.bind():
+    log.logger.info('Starting user sync')
+    sync_users()
+    log.logger.info('Starting group sync')
+    sync_groups()
+    log.logger.info('Starting sync users in groups')
+    sync_membership()
 c.unbind()
