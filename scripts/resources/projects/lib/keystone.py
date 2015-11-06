0c86887dc75836c4956133a96d65963fb8aee172
Atze de Vries <atze.devries@naturalis.nl>
2015-11-06 15:09:21 +0100

    jyg

--- scripts/resources/projects/lib/keystone.py
+++ scripts/resources/projects/lib/keystone.py
@@ -84,7 +84,7 @@ class KeyStone:
             except:
                 current_denied.append(g.name)
 
-        added = [a for a in group_names if (a not in current_access and a not excludes)]
+        added = [a for a in group_names if (a not in current_access and a not in excludes)]
         removed = [ r for r in group_names if (r not in current_denied and r not in excludes) ]
 
         print "ADDED: %s" % added
