5732adcc70ce973a9881244cd91be2a87ebed007
Atze de Vries <atze.devries@naturalis.nl>
2015-11-06 10:14:59 +0100

    some ordering

---
+++ scripts/resources/projects/testa.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python2.7
+
+
+from os import environ
+from lib import Ks
+
+try:
+    auth_url = environ['KS_ENDPOINT_V3']
+    ks_username = environ['OS_USERNAME']
+    ks_password = environ['OS_PASSWORD']
+    project_name = environ['OS_PROJECT_NAME']
+except KeyError as e:
+    print "ERROR: export of var KS_ENDPOINT_V3, OS_USERNAME, OS_PASSWORD and OS_PROJECT_NAME should exist"
+    exit(1)
+
+ks = Ks(auth_url,ks_username,ks_password,project_name)
+
+print ks.user_enabled('rutger.vos')
