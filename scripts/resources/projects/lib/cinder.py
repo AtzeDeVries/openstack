c30032e46ff41039b869418bc9f751464eb4dca4
Atze de Vries <atze.devries@naturalis.nl>
2015-11-11 10:09:45 +0100

    something weird

--- scripts/resources/projects/lib/cinder.py
+++ scripts/resources/projects/lib/cinder.py
@@ -7,7 +7,7 @@ from . import log
 
 class Cinder():
 
-    def __init__(self,auth_url,username,password,tenant_namem,cacert):
+    def __init__(self,auth_url,username,password,tenant_name,cacert):
         auth = v2.Password(auth_url=auth_url,
                            username=username,
                            password=password,
